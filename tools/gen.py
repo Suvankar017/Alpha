fa0478e87b8e1699552a126af445739e4492d920
Suvankar017 <yougeneral017@gmail.com>
2022-01-31 05:54:33 +0530

    Init Project

--- tools/gen.py
+++ tools/gen.py
@@ -6,10 +6,10 @@ res = 0
 if globals.IsWindows():
     res = subprocess.call(["cmd.exe", "/c", "{}\\premake5".format(globals.PREMAKE_DIR), globals.VS_VERSION])
 elif globals.IsLinux():
-    res = subprocess.call(["{}/premake5.linux".format(globals.PREMAKE_DIR), "gmake2"])
+    res = subprocess.call(["{}/premake5_linux.linux".format(globals.PREMAKE_DIR), "gmake2"])
 elif globals.IsMac():
-    res = subprocess.call(["{}/premake5".format(globals.PREMAKE_DIR), "gmake2"])
+    res = subprocess.call(["{}/premake5_mac".format(globals.PREMAKE_DIR), "gmake2"])
     if res == 0:
-        res = subprocess.call(["{}/premake5".format(globals.PREMAKE_DIR), "xcode4"])
+        res = subprocess.call(["{}/premake5_mac".format(globals.PREMAKE_DIR), "xcode4"])
 
 sys.exit(res)
